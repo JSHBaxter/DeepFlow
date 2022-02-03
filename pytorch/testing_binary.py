e00144d0e6c053f90a0708692c11d57732e7675f
John SH Baxter <jshbaxter@gmail.com>
2022-02-03 19:33:42 +0100

    Improved testing scripts for binary and potts

--- pytorch/testing_binary.py
+++ pytorch/testing_binary.py
@@ -1,3 +1,5 @@
+import unittest
+
 import numpy as np
 
 import time
@@ -7,126 +9,261 @@ from binary_deepflow import Binary_MAP1d,Binary_MAP2d,Binary_MAP3d
 from binary_deepflow import Binary_Mean1d,Binary_Mean2d,Binary_Mean3d
 
 b=1
-c=3
-x=32
-epsilon = 0.000000001
+c=4
+x=4096
+epsilon = 0.01
 
+def test_no_smoothness(d,device,asserter):
+    print("Testing (no smoothness) \t Dim: " +str(d)+ " \t Dev: " + device)
+    x_used = int(x**(1/d))
 
-def test_dimensionality(d):
-    
-    size_info = tuple( [b,c]+[x for i in range(d)] )
-    size_red_info = tuple( [1,c]+[x for i in range(d)] )
-    
-    data_t = (0.5*np.random.normal(0,1,size=size_info)).astype(np.float32)
-    data_r = (1*np.clip(0.25*np.random.normal(0,1,size=size_info)+np.array([i % 2 for i in range(c*x**d)]).reshape(size_red_info),0,100)).astype(np.float32)
+    size_info = tuple( [b,c]+[x_used for i in range(d)] )
+    size_red_info = tuple( [1,c]+[x_used for i in range(d)] )
 
-    t = torch.tensor(data_t, device=torch.device("cuda"))
-    r = torch.tensor(data_r, device=torch.device("cuda"))
+    data_t = np.random.normal(0,1,size=size_info).astype(np.float32)
+    data_w = np.random.normal(0,1,size=size_info).astype(np.float32)
+    data_rx = np.zeros(shape=size_info).astype(np.float32)
+    if d > 1:
+        data_ry = np.zeros(shape=size_info).astype(np.float32)
+    if d > 2:
+        data_rz = np.zeros(shape=size_info).astype(np.float32)
+
+    t = torch.tensor(data_t, device=torch.device(device))
     t.requires_grad = True
-    r.requires_grad = True
+    w = torch.tensor(data_w, device=torch.device(device))
+    rx = torch.tensor(data_rx, device=torch.device(device))
+    rx.requires_grad = True
+    if d > 1:
+        ry = torch.tensor(data_ry, device=torch.device(device))
+        ry.requires_grad = True
+    if d > 2:
+        rz = torch.tensor(data_rz, device=torch.device(device))
+        rz.requires_grad = True
 
     if d == 1:
-        oa_1 = torch.exp(Binary_MAP1d.apply(t,r))
-        om_1 = Binary_Mean1d.apply(t,r)
+        oa = torch.exp(Binary_MAP1d.apply(t,rx))
+        om = Binary_Mean1d.apply(t,rx)
     elif d == 2:
-        oa_1 = torch.exp(Binary_MAP2d.apply(t,r,r))
-        om_1 = Binary_Mean2d.apply(t,r,r)
+        oa = torch.exp(Binary_MAP2d.apply(t,rx,ry))
+        om = Binary_Mean2d.apply(t,rx,ry)
     elif d == 3:
-        oa_1 = torch.exp(Binary_MAP3d.apply(t,r,r,r))
-        om_1 = Binary_Mean3d.apply(t,r,r,r)
-
-
-        
-    print("\nGPU " + str(d) + "D results:")
-    print("data sum")
-    print(torch.sum(t,dim=2))
-    print("avg MAP")
-    print(torch.sum(oa_1,dim=2)/x)
-    print("data")
-    print(t)
-    print("reg")
-    print(r)
-    print("mean pass")
-    print(om_1)
-    print("data mean pass diff")
-    print((om_1-t))
-    print("MAP res")
-    print(oa_1)
-    
-    loss = -torch.sum(om_1)
+        oa = torch.exp(Binary_MAP3d.apply(t,rx,ry,rz))
+        om = Binary_Mean3d.apply(t,rx,ry,rz)
+    loss = torch.sum(w*om)
     loss.backward()
-
-    t_g1 = t.grad
-    r_g1 = r.grad
+    oa_np = oa.detach().cpu().numpy()
+    om_np = om.detach().cpu().numpy()
+    ot_np = t.grad.detach().cpu().numpy()
     
-    print("data grad")
-    print(t_g1)
-    print("reg grad")
-    print(r_g1)
-    print(torch.sum(r_g1))
+    #make sure not nan
+    asserter.assertFalse(np.any(np.isnan(oa_np)))
+    asserter.assertFalse(np.any(np.isnan(om_np)))
+    asserter.assertFalse(np.any(np.isnan(ot_np)))
+    
+    #ensure MAP assigns 1 to positive terms
+    for val_df, val_d in zip(oa_np.flatten(),data_t.flatten()):
+        asserter.assertFalse(val_d > epsilon and val_df < 1-epsilon)
+        asserter.assertFalse(val_d < -epsilon and val_df > epsilon)
+        
+    #ensure mean pass is equivalent to the data terms only
+    for val_df, val_d in zip(om_np.flatten(),data_t.flatten()):
+        asserter.assertTrue(abs(val_df-val_d) < epsilon)
+    
+    #ensure gradient wrt data terms are passed immediately through
+    for val_df, val_d in zip(ot_np.flatten(),data_w.flatten()):
+        asserter.assertTrue(abs(val_df-val_d) < epsilon)
 
+def test_smoothness_dom(d,device,asserter):
+    print("Testing (smoothness dom.) \t Dim: " +str(d)+ " \t Dev: " + device)
+    x_used = int(x**(1/d))
 
+    size_info = tuple( [b,c]+[x_used for i in range(d)] )
+    size_red_info = tuple( [1,c]+[x_used for i in range(d)] )
+    axes = tuple([i+2 for i in range(d)])
 
-    t = torch.tensor(data_t, device=torch.device("cpu")).contiguous()
-    r = torch.tensor(data_r, device=torch.device("cpu")).contiguous()
-    t.requires_grad = True
-    r.requires_grad = True
+    data_t = 0.1*np.random.normal(0,1,size=size_info).astype(np.float32)
+    data_t += epsilon / np.sum(data_t,axis=axes,keepdims=True)
+    data_rx = 250+25*np.random.uniform(size=size_info).astype(np.float32)
+    if d > 1:
+        data_ry = 250+25*np.random.uniform(size=size_info).astype(np.float32)
+    if d > 2:
+        data_rz = 250+25*np.random.uniform(size=size_info).astype(np.float32)
 
+    t = torch.tensor(data_t, device=torch.device(device))
+    rx = torch.tensor(data_rx, device=torch.device(device))
+    if d > 1:
+        ry = torch.tensor(data_ry, device=torch.device(device))
+    if d > 2:
+        rz = torch.tensor(data_rz, device=torch.device(device))
+        
+    
     if d == 1:
-        oa_2 = torch.exp(Binary_MAP1d.apply(t,r))
-        om_2 = Binary_Mean1d.apply(t,r)
+        oa = torch.exp(Binary_MAP1d.apply(t,rx))
+        om = Binary_Mean1d.apply(t,rx)
     elif d == 2:
-        oa_2 = torch.exp(Binary_MAP2d.apply(t,r,r))
-        om_2 = Binary_Mean2d.apply(t,r,r)
+        oa = torch.exp(Binary_MAP2d.apply(t,rx,ry))
+        om = Binary_Mean2d.apply(t,rx,ry)
     elif d == 3:
-        oa_2 = torch.exp(Binary_MAP3d.apply(t,r,r,r))
-        om_2 = Binary_Mean3d.apply(t,r,r,r)
-
-        
-    print("\nCPU " + str(d) + "D results:")
-    print("data sum")
-    print(torch.sum(t,dim=2))
-    print("avg MAP")
-    print(torch.sum(oa_2,dim=2)/x)
-    print("data")
-    print(t)
-    print("reg")
-    print(r)
-    print("mean pass")
-    print(om_2)
-    print("data mean pass diff")
-    print(om_2-t)
-    print("MAP res")
-    print(oa_2)
-
-    loss = -torch.sum(om_2)
-    loss.backward()
-
-    t_g2 = t.grad
-    r_g2 = r.grad
+        oa = torch.exp(Binary_MAP3d.apply(t,rx,ry,rz))
+        om = Binary_Mean3d.apply(t,rx,ry,rz)
+    oa_np = oa.detach().cpu().numpy()
+    om_np = om.detach().cpu().numpy()
     
-    print("data grad")
-    print(t_g2)
-    print("reg grad")
-    print(r_g2)
-    print(torch.sum(r_g2))
+    #make sure not nan
+    asserter.assertFalse(np.any(np.isnan(oa_np)))
+    asserter.assertFalse(np.any(np.isnan(om_np)))
+    
+    #ensure MAP assigns 1 to positive terms and mean shares sign with the sum of the data terms
+    totals = np.sum(data_t,axis=axes)
+    for i in range(c):
+        for val_df in oa_np[0,i,...].flatten():
+            asserter.assertFalse(totals[0,i] > epsilon and val_df < 1-epsilon)
+            asserter.assertFalse(totals[0,i] < -epsilon and val_df > epsilon)
+        for val_df in om_np[0,i,...].flatten():
+            asserter.assertFalse(totals[0,i] > epsilon and val_df < -epsilon)
+            asserter.assertFalse(totals[0,i] < -epsilon and val_df > epsilon)
+
+def test_device_equivalence(d,device_list,asserter):
+    print("Testing (dev equiv.) \t Dim: " +str(d)+ " \t Dev:",device_list)
+    x_used = int(x**(1/d))
+
+    size_info = tuple( [b,c]+[x_used for i in range(d)] )
+    size_red_info = tuple( [1,c]+[x_used for i in range(d)] )
+
+    data_t = 0*np.random.normal(0,1,size=size_info).astype(np.float32)
+    data_w = np.random.normal(0,1,size=size_info).astype(np.float32)
+    data_rx = (0.25*np.random.uniform(size=size_info)+0.00001).astype(np.float32)
+    if d > 1:
+        data_ry = (0.25*np.random.uniform(size=size_info)+0.00001).astype(np.float32)
+    if d > 2:
+        data_rz= (0.25*np.random.uniform(size=size_info)+0.00001).astype(np.float32)
+
+    res = {}
+    for device in device_list:
+        print("\tRunning device " + device)
+        t = torch.tensor(data_t, device=torch.device(device))
+        w = torch.tensor(data_w, device=torch.device(device))
+        t.requires_grad = True
+        
+        rx = torch.tensor(data_rx, device=torch.device(device))
+        rx.requires_grad = True
+        if d > 1:
+            ry = torch.tensor(data_ry, device=torch.device(device))
+            ry.requires_grad = True
+        if d > 2:
+            rz = torch.tensor(data_rz, device=torch.device(device))
+            rz.requires_grad = True
+            
+        if d == 1:
+            oa = torch.exp(Binary_MAP1d.apply(t,rx))
+            om = Binary_Mean1d.apply(t,rx)
+        elif d == 2:
+            oa = torch.exp(Binary_MAP2d.apply(t,rx,ry))
+            om = Binary_Mean2d.apply(t,rx,ry)
+        elif d == 3:
+            oa = torch.exp(Binary_MAP3d.apply(t,rx,ry,rz))
+            om = Binary_Mean3d.apply(t,rx,ry,rz)
+        loss = torch.sum(w*om)
+        loss.backward()
+
+        oa_np = oa.detach().cpu().numpy()
+        om_np = om.detach().cpu().numpy()
+        ot_np = t.grad.detach().cpu().numpy()
+        
+        #make sure not nan
+        asserter.assertFalse(np.any(np.isnan(oa_np)))
+        asserter.assertFalse(np.any(np.isnan(om_np)))
+        asserter.assertFalse(np.any(np.isnan(ot_np)))
+        res[device] = [om_np,ot_np,oa_np]
+        
+        orx_np = rx.grad.detach().cpu().numpy()
+        asserter.assertFalse(np.any(np.isnan(orx_np)))
+        orx_np = rx.grad.detach().cpu().numpy()
+        res[device].append(orx_np)
+        if d > 1:
+            ory_np = ry.grad.detach().cpu().numpy()
+            asserter.assertFalse(np.any(np.isnan(ory_np)))
+            ory_np = ry.grad.detach().cpu().numpy()
+            res[device].append(ory_np)
+        if d > 2:
+            orz_np = rz.grad.detach().cpu().numpy() 
+            asserter.assertFalse(np.any(np.isnan(orz_np)))
+            orz_np = rz.grad.detach().cpu().numpy()
+            res[device].append(orz_np)
+                           
+
+    name = ['om_np','ot_np','oa_np','orx_np']
+    epsilon_l = [0.1*epsilon,0.1*epsilon,0.1,0.1*epsilon]
+    if d > 1:
+        name.append('ory_np')
+        epsilon_l.append(0.1*epsilon)
+    if d > 2:
+        name.append('orz_np')
+        epsilon_l.append(0.1*epsilon)
+    for i,dev1 in enumerate(device_list):
+        for dev2 in device_list[(i+1):]:
+            diffs = [np.max(abs(res[dev1][j]-res[dev2][j])) for j in range(len(name))]
+            for var in range(len(name)):
+                if(diffs[var] > epsilon_l[var] or np.isnan(diffs[var])):
+                    raise Exception(name[var]+"\t"+str(diffs[var]))
+
+class Test_Extreme(unittest.TestCase):
+
+    def test_no_smoothness_1D(self):
+        print("")
+        if torch.has_cuda:
+            test_no_smoothness(1,"cuda",self)
+        test_no_smoothness(1,"cpu",self)
+
+    def test_no_smoothness_2D(self):
+        print("")
+        if torch.has_cuda:
+            test_no_smoothness(2,"cuda",self)
+        test_no_smoothness(2,"cpu",self)
+
+    def test_no_smoothness_3D(self):
+        print("")
+        if torch.has_cuda:
+            test_no_smoothness(3,"cuda",self)
+        test_no_smoothness(3,"cpu",self)
+
+    def test_smoothness_dom_1D(self):
+        print("")
+        if torch.has_cuda:
+            test_smoothness_dom(1,"cuda",self)
+        test_smoothness_dom(1,"cpu",self)
+
+    def test_smoothness_dom_2D(self):
+        print("")
+        if torch.has_cuda:
+            test_smoothness_dom(2,"cuda",self)
+        test_smoothness_dom(2,"cpu",self)
+
+    def test_smoothness_dom_3D(self):
+        print("")
+        if torch.has_cuda:
+            test_smoothness_dom(3,"cuda",self)
+        test_smoothness_dom(3,"cpu",self)
+            
+    def test_equivalence_1d(self):
+        print("")
+        if torch.has_cuda:
+            test_device_equivalence(1,["cuda","cpu"],self)
+            
+    def test_equivalence_2d(self):
+        print("")
+        if torch.has_cuda:
+            test_device_equivalence(2,["cuda","cpu"],self)
+            
+    def test_equivalence_3d(self):
+        print("")
+        if torch.has_cuda:
+            test_device_equivalence(3,["cuda","cpu"],self)
+        
+        
 
 
-    print("\nDifferences " + str(d) + "D:")
-    print("auglag res")
-    #print(2*(oa_1.cpu()-oa_2)/(abs(oa_1.cpu())+abs(oa_2)+epsilon))
-    print(torch.max(torch.abs((oa_1.cpu()-oa_2))))
-    print("meanpass res")
-    #print(2*(om_1.cpu()-om_2)/(abs(om_1.cpu())+abs(om_2)+epsilon))
-    print(torch.max(torch.abs((om_1.cpu()-om_2))))
-    print("t_g")
-    #print(2*(t_g1.cpu()-t_g2)/(abs(t_g1.cpu())+abs(t_g2)+epsilon))
-    print(torch.max(torch.abs((t_g1.cpu()-t_g2))))
-    print("r_g")
-    #print(2*(t_g1.cpu()-t_g2)/(abs(t_g1.cpu())+abs(t_g2)+epsilon))
-    print(torch.max(torch.abs((r_g1.cpu()-r_g2))))
-
-
-for d in range(1,2):
-    test_dimensionality(1)
+if __name__ == '__main__':
+    unittest.main()
+    
     
--- pytorch/testing_potts.py
+++ pytorch/testing_potts.py
@@ -1,3 +1,5 @@
+import unittest
+
 import numpy as np
 
 import time
@@ -7,126 +9,274 @@ from potts_deepflow import Potts_MAP1d,Potts_MAP2d,Potts_MAP3d
 from potts_deepflow import Potts_Mean1d,Potts_Mean2d,Potts_Mean3d
 
 b=1
-c=3
-x=5
-epsilon = 0.000000001
+c=5
+x= 4096
+epsilon = 0.01
 
+def test_no_smoothness(d,device,asserter):
+    print("Testing (no smoothness) \t Dim: " +str(d)+ " \t Dev: " + device)
+    x_used = int(x**(1/d))
 
-def test_dimensionality(d):
-    
-    size_info = tuple( [b,c]+[x for i in range(d)] )
-    size_red_info = tuple( [1,1]+[x for i in range(d)] )
-    
-    data_t = (0.5*np.random.normal(0,1,size=size_info)).astype(np.float32)
-    data_r = (1*np.clip(0.25*np.random.normal(0,1,size=size_info)+np.array([i % 2 for i in range(x**d)]).reshape(size_red_info),0,100)).astype(np.float32)
+    size_info = tuple( [b,c]+[x_used for i in range(d)] )
+    size_red_info = tuple( [1,c]+[x_used for i in range(d)] )
 
-    t = torch.tensor(data_t, device=torch.device("cuda"))
-    r = torch.tensor(data_r, device=torch.device("cuda"))
+    data_t = np.random.normal(0,1,size=size_info).astype(np.float32)
+    data_w = np.random.normal(0,1,size=size_info).astype(np.float32)
+    data_rx = np.zeros(shape=size_info).astype(np.float32)
+    if d > 1:
+        data_ry = np.zeros(shape=size_info).astype(np.float32)
+    if d > 2:
+        data_rz = np.zeros(shape=size_info).astype(np.float32)
+
+    t = torch.tensor(data_t, device=torch.device(device))
     t.requires_grad = True
-    r.requires_grad = True
+    w = torch.tensor(data_w, device=torch.device(device))
+    rx = torch.tensor(data_rx, device=torch.device(device))
+    rx.requires_grad = True
+    if d > 1:
+        ry = torch.tensor(data_ry, device=torch.device(device))
+        ry.requires_grad = True
+    if d > 2:
+        rz = torch.tensor(data_rz, device=torch.device(device))
+        rz.requires_grad = True
 
     if d == 1:
-        oa_1 = torch.exp(Potts_MAP1d.apply(t,r))
-        om_1 = Potts_Mean1d.apply(t,r)
+        oa = torch.exp(Potts_MAP1d.apply(t,rx))
+        om = Potts_Mean1d.apply(t,rx)
     elif d == 2:
-        oa_1 = torch.exp(Potts_MAP2d.apply(t,r,r))
-        om_1 = Potts_Mean2d.apply(t,r,r)
+        oa = torch.exp(Potts_MAP2d.apply(t,rx,ry))
+        om = Potts_Mean2d.apply(t,rx,ry)
     elif d == 3:
-        oa_1 = torch.exp(Potts_MAP3d.apply(t,r,r,r))
-        om_1 = Potts_Mean3d.apply(t,r,r,r)
-
-
-        
-    print("\nGPU " + str(d) + "D results:")
-    print("data sum")
-    print(torch.sum(t,dim=2))
-    print("avg MAP")
-    print(torch.sum(oa_1,dim=2)/x)
-    print("data")
-    print(t)
-    print("reg")
-    print(r)
-    print("mean pass")
-    print(om_1)
-    print("data mean pass diff")
-    print((om_1-t))
-    print("MAP res")
-    print(oa_1)
-    
-    loss = -torch.sum(om_1.flatten()[0])
+        oa = torch.exp(Potts_MAP3d.apply(t,rx,ry,rz))
+        om = Potts_Mean3d.apply(t,rx,ry,rz)
+    loss = torch.sum(w*om)
     loss.backward()
-
-    t_g1 = t.grad
-    r_g1 = r.grad
+    oa_np = oa.detach().cpu().numpy()
+    om_np = om.detach().cpu().numpy()
+    ot_np = t.grad.detach().cpu().numpy()
     
-    print("data grad")
-    print(t_g1)
-    print("reg grad")
-    print(r_g1)
-    print(torch.sum(r_g1))
+    #make sure not nan
+    asserter.assertFalse(np.any(np.isnan(oa_np)))
+    asserter.assertFalse(np.any(np.isnan(om_np)))
+    asserter.assertFalse(np.any(np.isnan(ot_np)))
+    
+    #resize into more usable form
+    dt_np_l = [data_t[0,i,...].flatten() for i in range(c)]
+    dw_np_l = [data_w[0,i,...].flatten() for i in range(c)]
+    oa_np_l = [oa_np[0,i,...].flatten()  for i in range(c)]
+    om_np_l = [om_np[0,i,...].flatten()  for i in range(c)]
+    ot_np_l = [ot_np[0,i,...].flatten()  for i in range(c)]
+    x_space = len(dt_np_l[0])
+    
+    #ensure MAP assigns 1 to highest term and 0 to everything else
+    for i in range(x_space):
+        highest = max([o[i] for o in dt_np_l])
+        for ic in range(c):
+            if(dt_np_l[ic][i] == highest and oa_np_l[ic][i] < 0.5):
+                raise Exception(str(dt_np_l[ic][i])+"\t"+str([o[i] for o in dt_np_l])+"\t"+str(highest)+"\t"+str(oa_np_l[ic][i])+"\t"+str([o[i] for o in oa_np_l]))
+            if(dt_np_l[ic][i] < highest - epsilon  and oa_np_l[ic][i] > 0.5):
+                raise Exception(str(dt_np_l[ic][i])+"\t"+str([o[i] for o in dt_np_l])+"\t"+str(highest)+"\t"+str(oa_np_l[ic][i])+"\t"+str([o[i] for o in oa_np_l]))
+        
+    #ensure mean pass is equivalent to the data terms only
+    for i in range(c): 
+        for val_df, val_d in zip(om_np_l[i],dt_np_l[i]):
+            if(abs(val_df-val_d) > epsilon):
+                raise Exception(str(val_df) + "\t" + str(val_d))
+    
+    #ensure gradient wrt data terms are passed immediately through
+    for i in range(c): 
+        for val_df, val_d in zip(ot_np_l[i],dw_np_l[i]):
+            if(abs(val_df-val_d) > epsilon):
+                raise Exception(str(val_df) + "\t" + str(val_d))
 
+def test_smoothness_dom(d,device,asserter):
+    print("Testing (smoothness dom.) \t Dim: " +str(d)+ " \t Dev: " + device)
+    x_used = int(x**(1/d))
 
+    size_info = tuple( [b,c]+[x_used for i in range(d)] )
+    size_red_info = tuple( [1,c]+[x_used for i in range(d)] )
+    axes = tuple([i+2 for i in range(d)])
 
-    t = torch.tensor(data_t, device=torch.device("cpu")).contiguous()
-    r = torch.tensor(data_r, device=torch.device("cpu")).contiguous()
-    t.requires_grad = True
-    r.requires_grad = True
+    winner = int(np.random.uniform()*c)
+    data_t = 0.01*np.random.uniform(0,1,size=size_info).astype(np.float32)
+    data_t[:,winner,...] += 0.005
+    data_r = 1+1*np.random.uniform(size=size_info).astype(np.float32)
 
+    t = torch.tensor(data_t, device=torch.device(device))
+    r = torch.tensor(data_r, device=torch.device(device))
+    
     if d == 1:
-        oa_2 = torch.exp(Potts_MAP1d.apply(t,r))
-        om_2 = Potts_Mean1d.apply(t,r)
+        oa = torch.exp(Potts_MAP1d.apply(t,r))
+        om = Potts_Mean1d.apply(t,r)
     elif d == 2:
-        oa_2 = torch.exp(Potts_MAP2d.apply(t,r,r))
-        om_2 = Potts_Mean2d.apply(t,r,r)
+        oa = torch.exp(Potts_MAP2d.apply(t,r,r))
+        om = Potts_Mean2d.apply(t,r,r)
     elif d == 3:
-        oa_2 = torch.exp(Potts_MAP3d.apply(t,r,r,r))
-        om_2 = Potts_Mean3d.apply(t,r,r,r)
-
-        
-    print("\nCPU " + str(d) + "D results:")
-    print("data sum")
-    print(torch.sum(t,dim=2))
-    print("avg MAP")
-    print(torch.sum(oa_2,dim=2)/x)
-    print("data")
-    print(t)
-    print("reg")
-    print(r)
-    print("mean pass")
-    print(om_2)
-    print("data mean pass diff")
-    print(om_2-t)
-    print("MAP res")
-    print(oa_2)
-
-    loss = -torch.sum(om_2.flatten()[0])
-    loss.backward()
-
-    t_g2 = t.grad
-    r_g2 = r.grad
+        oa = torch.exp(Potts_MAP3d.apply(t,r,r,r))
+        om = Potts_Mean3d.apply(t,r,r,r)
+    oa_np = oa.detach().cpu().numpy()
+    om_np = om.detach().cpu().numpy()
     
-    print("data grad")
-    print(t_g2)
-    print("reg grad")
-    print(r_g2)
-    print(torch.sum(r_g2))
+    #make sure not nan
+    asserter.assertFalse(np.any(np.isnan(oa_np)))
+    asserter.assertFalse(np.any(np.isnan(om_np)))
+    
+    #resize into more usable form
+    dt_np_l = [data_t[0,i,...].flatten() for i in range(c)]
+    oa_np_l = [oa_np[0,i,...].flatten()  for i in range(c)]
+    om_np_l = [om_np[0,i,...].flatten()  for i in range(c)]
+    x_space = len(dt_np_l[0])
+    
+    #ensure MAP assigns 1 to highest terms
+    sums = [np.sum(o) for o in dt_np_l]
+    highest = max(sums)
+    for i in range(c):
+        for val_df in oa_np[0,i,...].flatten():
+            if(sums[i] == highest and val_df < 0.5):
+                raise Exception(str(sums[i])+ "\t" + str(sums) + "\t" + str(val_df))
+            if(sums[i] < highest  and val_df > 0.5):
+                raise Exception(str(sums[i])+ "\t" + str(sums) + "\t" + str(val_df))
+
+def test_device_equivalence(d,device_list,asserter):
+    print("Testing (dev equiv.) \t Dim: " +str(d)+ " \t Dev:",device_list)
+    x_used = int(x**(1/d))
+
+    size_info = tuple( [b,c]+[x_used for i in range(d)] )
+    size_red_info = tuple( [1,c]+[x_used for i in range(d)] )
+
+    data_t = 0*np.random.normal(0,1,size=size_info).astype(np.float32)
+    data_w = np.random.normal(0,1,size=size_info).astype(np.float32)
+    data_rx = (0.25*np.random.uniform(size=size_info)+0.00001).astype(np.float32)
+    if d > 1:
+        data_ry = (0.25*np.random.uniform(size=size_info)+0.00001).astype(np.float32)
+    if d > 2:
+        data_rz= (0.25*np.random.uniform(size=size_info)+0.00001).astype(np.float32)
+
+    res = {}
+    for device in device_list:
+        t = torch.tensor(data_t, device=torch.device(device))
+        w = torch.tensor(data_w, device=torch.device(device))
+        t.requires_grad = True
+        
+        rx = torch.tensor(data_rx, device=torch.device(device))
+        rx.requires_grad = True
+        if d > 1:
+            ry = torch.tensor(data_ry, device=torch.device(device))
+            ry.requires_grad = True
+        if d > 2:
+            rz = torch.tensor(data_rz, device=torch.device(device))
+            rz.requires_grad = True
+            
+        if d == 1:
+            oa = torch.exp(Potts_MAP1d.apply(t,rx))
+            om = Potts_Mean1d.apply(t,rx)
+        elif d == 2:
+            oa = torch.exp(Potts_MAP2d.apply(t,rx,ry))
+            om = Potts_Mean2d.apply(t,rx,ry)
+        elif d == 3:
+            oa = torch.exp(Potts_MAP3d.apply(t,rx,ry,rz))
+            om = Potts_Mean3d.apply(t,rx,ry,rz)
+        loss = torch.sum(w*om)
+        loss.backward()
+
+        oa_np = oa.detach().cpu().numpy()
+        om_np = om.detach().cpu().numpy()
+        ot_np = t.grad.detach().cpu().numpy()
+        
+        #make sure not nan
+        asserter.assertFalse(np.any(np.isnan(oa_np)))
+        asserter.assertFalse(np.any(np.isnan(om_np)))
+        asserter.assertFalse(np.any(np.isnan(ot_np)))
+        res[device] = [om_np,ot_np,oa_np]
+        
+        orx_np = rx.grad.detach().cpu().numpy()
+        asserter.assertFalse(np.any(np.isnan(orx_np)))
+        orx_np = rx.grad.detach().cpu().numpy()
+        res[device].append(orx_np)
+        if d > 1:
+            ory_np = ry.grad.detach().cpu().numpy()
+            asserter.assertFalse(np.any(np.isnan(ory_np)))
+            ory_np = ry.grad.detach().cpu().numpy()
+            res[device].append(ory_np)
+        if d > 2:
+            orz_np = rz.grad.detach().cpu().numpy() 
+            asserter.assertFalse(np.any(np.isnan(orz_np)))
+            orz_np = rz.grad.detach().cpu().numpy()
+            res[device].append(orz_np)
+                           
+
+    name = ['om_np','ot_np','oa_np','orx_np']
+    epsilon_l = [0.1*epsilon,0.1*epsilon,0.1,0.1*epsilon]
+    if d > 1:
+        name.append('ory_np')
+        epsilon_l.append(0.1*epsilon)
+    if d > 2:
+        name.append('orz_np')
+        epsilon_l.append(0.1*epsilon)
+    for i,dev1 in enumerate(device_list):
+        for dev2 in device_list[(i+1):]:
+            diffs = [np.max(abs(res[dev1][j]-res[dev2][j])) for j in range(len(name))]
+            for var in range(len(name)):
+                if(diffs[var] > epsilon_l[var] or np.isnan(diffs[var])):
+                    raise Exception(name[var]+"\t"+str(diffs[var]))
+
+class Test_Extreme(unittest.TestCase):
+
+    def test_no_smoothness_1D(self):
+        print("")
+        if torch.has_cuda:
+            test_no_smoothness(1,"cuda",self)
+        test_no_smoothness(1,"cpu",self)
+
+    def test_no_smoothness_2D(self):
+        print("")
+        if torch.has_cuda:
+            test_no_smoothness(2,"cuda",self)
+        test_no_smoothness(2,"cpu",self)
+
+    def test_no_smoothness_3D(self):
+        print("")
+        if torch.has_cuda:
+            test_no_smoothness(3,"cuda",self)
+        test_no_smoothness(3,"cpu",self)
+
+    def test_smoothness_dom_1D(self):
+        print("")
+        if torch.has_cuda:
+            test_smoothness_dom(1,"cuda",self)
+        test_smoothness_dom(1,"cpu",self)
+
+    def test_smoothness_dom_2D(self):
+        print("")
+        if torch.has_cuda:
+            test_smoothness_dom(2,"cuda",self)
+        test_smoothness_dom(2,"cpu",self)
+
+    def test_smoothness_dom_3D(self):
+        print("")
+        if torch.has_cuda:
+            test_smoothness_dom(3,"cuda",self)
+        test_smoothness_dom(3,"cpu",self)
+            
+    def test_equivalence_1d(self):
+        print("")
+        if torch.has_cuda:
+            test_device_equivalence(1,["cpu","cuda"],self)
+            
+    def test_equivalence_2d(self):
+        print("")
+        if torch.has_cuda:
+            test_device_equivalence(2,["cpu","cuda"],self)
+            
+    def test_equivalence_3d(self):
+        print("")
+        if torch.has_cuda:
+            test_device_equivalence(3,["cpu","cuda"],self)
+        
+        
 
 
-    print("\nDifferences " + str(d) + "D:")
-    print("auglag res")
-    #print(2*(oa_1.cpu()-oa_2)/(abs(oa_1.cpu())+abs(oa_2)+epsilon))
-    print(torch.max(torch.abs((oa_1.cpu()-oa_2))))
-    print("meanpass res")
-    #print(2*(om_1.cpu()-om_2)/(abs(om_1.cpu())+abs(om_2)+epsilon))
-    print(torch.max(torch.abs((om_1.cpu()-om_2))))
-    print("t_g")
-    #print(2*(t_g1.cpu()-t_g2)/(abs(t_g1.cpu())+abs(t_g2)+epsilon))
-    print(torch.max(torch.abs((t_g1.cpu()-t_g2))))
-    print("r_g")
-    #print(2*(t_g1.cpu()-t_g2)/(abs(t_g1.cpu())+abs(t_g2)+epsilon))
-    print(torch.max(torch.abs((r_g1.cpu()-r_g2))))
-
-
-for d in range(1,2):
-    test_dimensionality(3)
+if __name__ == '__main__':
+    unittest.main()
+    
     
